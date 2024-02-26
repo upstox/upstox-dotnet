74117ecf6b5aacb378e218d8492c713acc6b239d
ketangupta12 <ketan.gupta@rksv.in>
2024-02-26 11:16:36 +0530

    init commit, added examples foler

---
+++ examples/websocket/market_data/MarketDataFeedClientApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Com.Upstox.Marketdatafeeder.Rpc.Proto;
+
+class WebSocketClientExample
+{
+    private const string WebSocketUri = "wss://api.upstox.com/v2/feed/market-data-feed";
+    private const string AccessToken = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI3UEJDNkQiLCJqdGkiOiI2NWRjMjQ4NjI3ZWFkOTMzYjUzODhmYWQiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNBY3RpdmUiOnRydWUsInNjb3BlIjpbImludGVyYWN0aXZlIiwiaGlzdG9yaWNhbCJdLCJpYXQiOjE3MDg5MjYwODYsImlzcyI6InVkYXBpLWdhdGV3YXktc2VydmljZSIsImV4cCI6MTcwODk4NDgwMH0.ICP337V_C5u0Mucb5EoTIeILd2oAiYfQ5Gofx8SrRr4";
+    static async Task Main(string[] args)
+    {
+        using (ClientWebSocket webSocket = new ClientWebSocket())
+        {
+            // Configure the WebSocket headers as needed for authorization
+            webSocket.Options.SetRequestHeader("Authorization", $"Bearer {AccessToken}");
+
+            // Connect to the WebSocket server
+            Uri serverUri = new Uri(WebSocketUri);
+            await webSocket.ConnectAsync(serverUri, CancellationToken.None);
+            Console.WriteLine("Connected to the server");
+
+            // Send subscription message
+            await SendSubscriptionMessage(webSocket);
+
+            // Receive data
+            await ReceiveData(webSocket);
+
+            // Close the WebSocket connection
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+        }
+    }
+
+    static async Task SendSubscriptionMessage(ClientWebSocket webSocket)
+    {
+        var message = new
+        {
+            guid = "someguid",
+            method = "sub",
+            data = new
+            {
+                mode = "full",
+                instrumentKeys = new[] { "NSE_INDEX|Nifty Bank", "NSE_INDEX|Nifty 50" }
+            }
+        };
+
+        string messageJson = System.Text.Json.JsonSerializer.Serialize(message);
+        byte[] messageBytes = Encoding.UTF8.GetBytes(messageJson);
+        await webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Binary, true, CancellationToken.None);
+        Console.WriteLine("Subscription message sent");
+    }
+
+    static async Task ReceiveData(ClientWebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4]; // Adjust the buffer size as needed
+
+        try
+        {
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Binary && result.Count > 0)
+                {
+                    var feedResponse = FeedResponse.Parser.ParseFrom(buffer, 0, result.Count);
+                    Console.WriteLine(feedResponse);
+                }
+                else if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                    Console.WriteLine("WebSocket connection closed.");
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception in ReceiveData: " + ex.Message);
+        }
+    }
+}
---
+++ examples/websocket/order_updates/OrderUpdatesFeedClientApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+public class WebsocketClientExample
+{
+    private const string WebSocketUri = "wss://api.upstox.com/v2/feed/portfolio-stream-feed";
+    private const string AccessToken = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI3UEJDNkQiLCJqdGkiOiI2NWRjMjQ4NjI3ZWFkOTMzYjUzODhmYWQiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNBY3RpdmUiOnRydWUsInNjb3BlIjpbImludGVyYWN0aXZlIiwiaGlzdG9yaWNhbCJdLCJpYXQiOjE3MDg5MjYwODYsImlzcyI6InVkYXBpLWdhdGV3YXktc2VydmljZSIsImV4cCI6MTcwODk4NDgwMH0.ICP337V_C5u0Mucb5EoTIeILd2oAiYfQ5Gofx8SrRr4";
+    
+    static async Task Main(string[] args)
+    {
+        using (ClientWebSocket webSocket = new ClientWebSocket())
+        {
+            // Configure the WebSocket headers as needed for authorization
+            webSocket.Options.SetRequestHeader("Authorization", $"Bearer {AccessToken}");
+
+            // Connect to the WebSocket server
+            Uri serverUri = new Uri(WebSocketUri);
+            await webSocket.ConnectAsync(serverUri, CancellationToken.None);
+            Console.WriteLine("Connected to the server");
+
+            // Receive data
+            await ReceiveData(webSocket);
+
+            // Close the WebSocket connection
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+        }
+    }
+
+    static async Task ReceiveData(ClientWebSocket webSocket)
+    {
+        try
+        {
+            while (webSocket.State == WebSocketState.Open)
+            {
+                ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+                WebSocketReceiveResult result = webSocket.ReceiveAsync(buffer, CancellationToken.None).Result;
+                byte[] byteArray = new byte[buffer.Count];
+                Array.Copy(buffer.Array, buffer.Offset, byteArray, 0, buffer.Count);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    string message = System.Text.Encoding.UTF8.GetString(byteArray, 0, result.Count);
+                    Console.WriteLine("Received: " + message);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception in ReceiveData: " + ex.Message);
+        }
+    }
+}
+
+
